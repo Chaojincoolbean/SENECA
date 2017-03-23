aa2e12e248db56ccf6513b82acf9291acf9a5428
Chaojin <jinchaoonly@gmail.com>
2017-03-23 15:38:29 -0400

    Merging project prep

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,15 +5,36 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 
 	public GameObject Player;
+	GameObject Mom;
+	float x;
+	float y;
+
 
 
 	// Use this for initialization
 	void Start () {
-		
+		Mom = Instantiate(Resources.Load("Prefabs/Mom", typeof(GameObject))) as GameObject;
+
+		Mom.gameObject.transform.position = new Vector3 (-10f, -3.5f, 0);
+
+		x = Mom.gameObject.transform.position.x;
+		y = Mom.gameObject.transform.position.y;
+		Debug.Log ("x:"+x+"y"+y);
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+
+		x = x + 0.01f;
+
+		Mom.gameObject.transform.position = new Vector3 (x, -3.5f, 0);
+
+		//Debug.Log (x);
+
 	}
+
+
+
 }
