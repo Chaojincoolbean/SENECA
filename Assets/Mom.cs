f6c01ddce3077d75f4773ff103e2d5ebfd1877ee
Chaojin <jinchaoonly@gmail.com>
2017-03-28 14:20:06 -0400

    forest1 scene and script

---
+++ Assets/Mom.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mom : MonoBehaviour {
+
+	float x;
+	bool onposition;
+
+	// Use this for initialization
+	void Start () {
+
+		x = -10f;
+		onposition = false;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (onposition == false) {
+
+			x = x + 0.05f;
+
+			this.gameObject.transform.position = new Vector3 (x, -3.5f, 0);
+
+		
+		}
+
+//		if (onposition == true) {
+//		
+//			x = x - 0.05f;
+//
+//			this.gameObject.transform.position = new Vector3 (x, -3.5f, 0);
+//
+//		}
+
+		if (x < -10f) {
+			Destroy (this);
+		
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+			
+
+		this.gameObject.GetComponent<AudioSource> ().Play ();
+
+		onposition = true;
+
+
+		Debug.Log (x);
+		//onposition = true;
+
+
+	}
+
+
+
+}
