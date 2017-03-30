87de5ceb1540d0345c2a561ead0c20407dae8179
Chaojin <jinchaoonly@gmail.com>
2017-03-30 15:04:14 -0400

    witchlight works now

---
+++ Assets/witchlightmanager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class witchlightmanager : MonoBehaviour {
+
+	public float x;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		x = this.gameObject.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (x - player.gameObject.transform.position.x < 2f) {
+
+			x = x + 0.05f;
+
+			this.gameObject.transform.position = new Vector3 (x, this.gameObject.transform.position.y, 0);
+		}
+
+		if (player.gameObject.transform.position.x - x > 0f ){
+
+			x = player.gameObject.transform.position.x + 2f;
+		}
+	}
+}
