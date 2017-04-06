7b8b3d3948d35d43f3258639214ec0f8f8817e81
Chaojin <jinchaoonly@gmail.com>
2017-04-05 21:11:46 -0400

    NPC movement！

---
+++ SENECA/Assets/Scripts/Game_World/NPC.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+	private Vector3 startPos;
+	private Vector3 endPos;
+	public float distance;
+	public float lerptime;
+	private float currentLerptime;
+	private bool n;
+
+
+	// Use this for initialization
+	void Start () {
+
+		startPos = this.gameObject.transform.position;
+		endPos = this.gameObject.transform.position + Vector3.right * distance;
+
+
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		wheretostart ();
+
+		if (n == true) {
+
+			currentLerptime += Time.deltaTime;
+			if (currentLerptime >= lerptime) {
+				currentLerptime = lerptime;
+			}
+
+			float Perc = currentLerptime / lerptime;
+			this.transform.position = Vector3.Lerp (startPos, endPos, Perc);
+			Debug.Log ("currenttime:" + currentLerptime);
+		}
+
+		if (n == false) {
+
+			currentLerptime += Time.deltaTime;
+			if (currentLerptime >= lerptime) {
+				currentLerptime = lerptime;
+			}
+
+			float Perc = currentLerptime / lerptime;
+			this.transform.position = Vector3.Lerp (endPos, startPos, Perc);
+			Debug.Log ("currenttime:" + currentLerptime);
+		}
+
+			
+		
+	}
+
+	void wheretostart(){
+
+		if (this.gameObject.transform.position == startPos) {
+			n = true;
+			currentLerptime = 0;
+		}
+
+		if (this.gameObject.transform.position == endPos) {
+			n = false;
+			currentLerptime = 0;
+		}
+		
+	}
+}
