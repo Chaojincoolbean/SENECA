f158fbc143f6f9b70753755cc13b17a6089ea357
Chaojin <jinchaoonly@gmail.com>
2017-05-06 14:25:00 -0400

    Skeleton for scene script made

--- SENECA/Assets/Scripts/Game_World/SceneScripts/TitleSceneScript.cs
+++ SENECA/Assets/Scripts/Game_World/SceneScripts/TitleSceneScript.cs
@@ -1,16 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GameScenes;
 
-public class TitleSceneScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+public class TitleSceneScript : Scene<TransitionData> 
+{
+	void OnEnter(TransitionData data)
+	{
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void OnExit()
+	{
 	}
 }
