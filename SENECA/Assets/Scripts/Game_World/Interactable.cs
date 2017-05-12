c7e12e2c1310e9700ebacc8b6ae9fee5e9836d3d
Christopher Williams <c.j.williams717@gmail.com>
2017-05-11 23:31:09 -0400

    Interactable.cs push

--- SENECA/Assets/Scripts/Game_World/Interactable.cs
+++ SENECA/Assets/Scripts/Game_World/Interactable.cs
@@ -12,15 +12,48 @@ public class Interactable : MonoBehaviour
 	// Use this for initialization
 	void Start () {
 		myAudioSource = GetComponent<AudioSource> ();
-		clip = Resources.Load ("Audio/VO/Priya/SCENE_1/VO_EVENT/Priya_Hurry") as AudioClip;
+		
 	}
 
 	void OnMouseDown()
 	{
 		if (transform.name == "Priya" && !myAudioSource.isPlaying) 
 		{
-			//myAudioSource.PlayOneShot (clip);
-		}
-	}
+            clip = Resources.Load("Audio/VO/Priya/SCENE_1/VO_EVENT/Priya_Hurry") as AudioClip;
+            myAudioSource.PlayOneShot (clip);
+		}   
+        else if(tag == "Rocks")
+        {
+            clip = Resources.Load("Audio/VO/Astrid/SCENE_1/VO_EVENT/Astrid_Rocks") as AudioClip;
+        }
+        else if (tag == "Radio")
+        {
+            clip = Resources.Load("Audio/VO/Astrid/SCENE_1/VO_EVENT/Astrid_Radio") as AudioClip;
+        }
+        else if (tag == "Rocks")
+        {
+            clip = Resources.Load("Audio/VO/Astrid/SCENE_1/VO_EVENT/Astrid_Rocks") as AudioClip;
+        }
+        else if (tag == "Sign")
+        {
+            clip = Resources.Load("Audio/VO/Astrid/SCENE_1/VO_EVENT/Astrid_Sign") as AudioClip;
+        }
+        else if (tag == "Racks")
+        {
+            clip = Resources.Load("Audio/VO/Astrid/SCENE_1/VO_EVENT/Astrid_Racks") as AudioClip;
+        }
+        else if (tag == "Fence")
+        {
+            clip = Resources.Load("Audio/VO/Astrid/SCENE_1/VO_EVENT/Astrid_Fence") as AudioClip;
+        }
+        else if (tag == "Carving")
+        {
+            clip = Resources.Load("Audio/VO/Astrid/SCENE_1/VO_EVENT/Astrid_Carving") as AudioClip;
+        }
+        else if (tag == "Backpack")
+        {
+            clip = Resources.Load("Audio/VO/Astrid/SCENE_1/VO_EVENT/Astrid_Backpack") as AudioClip;
+        }
+    }
 	
 }
