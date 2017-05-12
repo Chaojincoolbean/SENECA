a223ef112a76d8889399c04510d6517ecc84c4a6
Christopher Williams <c.j.williams717@gmail.com>
2017-05-12 00:37:21 -0400

    TransisitionData and Exit Script updated to support adding new scenes

--- SENECA/Assets/Scripts/ChrsUtils/SceneManager/TransitionData.cs
+++ SENECA/Assets/Scripts/ChrsUtils/SceneManager/TransitionData.cs
@@ -16,14 +16,24 @@ public class TransitionData
 
     public TitleScreen TITLE;
     public PrologueScreen PROLOUGE;
+
     public SenecaCampsite SENECA_CAMPSITE;
     public SenecaFork SENECA_FORK;
-    public SenecaMeadow SENECA_MEADOW;
+    public SenecaFarm SENECA_FARM;
+    public SenecaHunterCamp SENECA_HUNTER_CAMP;
+    public SenecaRocks SENECA_ROCKS;
+    public SenecaRadioTower SENECA_RADIO_TOWER;
     public SenecaRoad SENECA_ROAD;
-    public UtanMeadow UTAN_MEADOW;
-    public UtanRoad UTAN_ROAD;
+    public SenecaMeadow SENECA_MEADOW;
+
+    public UtanCampsite UTAN_CAMPSITE;
     public UtanFork UTAN_FORK;
-	public UtanRadioTower UTAN_RADIOTOWER;
+    public UtanFarm UTAN_FARM;
+    public UtanHunterCamp UTAN_HUNTER_CAMP;
+    public UtanRocks UTAN_ROCKS;
+    public UtanRadioTower UTAN_RADIO_TOWER;
+    public UtanRoad UTAN_ROAD;
+    public UtanMeadow UTAN_MEADOW;
 
 
     public readonly Dictionary<string, bool> difficulty;
@@ -55,37 +65,77 @@ public class TransitionData
            SENECA_FORK.position = _position;
            SENECA_FORK.scale = _scale;
        }
+       else if (scene == "Seneca_Farm")
+       {
+           SENECA_FARM.position = _position;
+           SENECA_FARM.scale = _scale;
+       }
+       else if (scene == "Seneca_HunterCamp")
+       {
+           SENECA_HUNTER_CAMP.position = _position;
+           SENECA_HUNTER_CAMP.scale = _scale;
+       }
+       else if (scene == "Seneca_Rocks")
+       {
+           SENECA_ROCKS.position = _position;
+           SENECA_ROCKS.scale = _scale;
+       }
+       else if (scene == "Seneca_Road")
+       {
+           SENECA_ROAD.position = _position;
+           SENECA_ROAD.scale = _scale;
+       }
+       else if (scene == "Seneca_Radio")
+       {
+           SENECA_RADIO_TOWER.position = _position;
+           SENECA_RADIO_TOWER.scale = _scale;
+       }
        else if(scene == "Seneca_Meadow")
        {
            SENECA_MEADOW.position = _position;
            SENECA_MEADOW.scale = _scale;
        }
-       else if(scene == "Seneca_Road")
-       {
-           SENECA_ROAD.position = _position;
-           SENECA_ROAD.scale = _scale;
-       }
-       else if(scene == "Utan_Meadow")
-       {
-           UTAN_MEADOW.position = _position;
-           UTAN_MEADOW.scale = _scale;
-       }
-       else if(scene == "Utan_Road")
-       {
-           UTAN_ROAD.position = _position;
-           UTAN_ROAD.scale = _scale;
-       }
-       else if(scene == "Utan_Fork")
-       {
-           UTAN_FORK.position = _position;
-           UTAN_FORK.scale = _scale;
-       }
-		else if(scene == "Utan_RadioTower")
-		{
-			UTAN_RADIOTOWER.position = _position;
-			UTAN_RADIOTOWER.scale = _scale;
-		}
-	
+        else if (scene == "Utan_Campsite")
+        {
+            UTAN_CAMPSITE.position = _position;
+            UTAN_CAMPSITE.scale = _scale;
+        }
+        else if (scene == "Utan_Fork")
+        {
+            UTAN_FORK.position = _position;
+            UTAN_FORK.scale = _scale;
+        }
+        else if (scene == "Utan_Farm")
+        {
+            UTAN_FARM.position = _position;
+            UTAN_FARM.scale = _scale;
+        }
+        else if (scene == "Utan_HunterCamp")
+        {
+            UTAN_HUNTER_CAMP.position = _position;
+            UTAN_HUNTER_CAMP.scale = _scale;
+        }
+        else if (scene == "Utan_Rocks")
+        {
+            UTAN_ROCKS.position = _position;
+            UTAN_ROCKS.scale = _scale;
+        }
+        else if (scene == "Utan_Road")
+        {
+            UTAN_ROAD.position = _position;
+            UTAN_ROAD.scale = _scale;
+        }
+        else if (scene == "Utan_Radio")
+        {
+            UTAN_RADIO_TOWER.position = _position;
+            UTAN_RADIO_TOWER.scale = _scale;
+        }
+        else if (scene == "Utan_Meadow")
+        {
+            UTAN_MEADOW.position = _position;
+            UTAN_MEADOW.scale = _scale;
+        }
+
     }
 
     public struct TitleScreen
@@ -116,45 +166,101 @@ public class TransitionData
         public Vector3 scale;
      }
 
-     public struct SenecaMeadow
+    public struct SenecaFarm
+    {
+        public bool visitedScene;
+        public Vector3 position;
+        public Vector3 scale;
+    }
+
+    public struct SenecaHunterCamp
+    {
+        public bool visitedScene;
+        public Vector3 position;
+        public Vector3 scale;
+    }
+
+    public struct SenecaRocks
+    {
+        public bool visitedScene;
+        public Vector3 position;
+        public Vector3 scale;
+    }
+
+    public struct SenecaRoad
+    {
+        public bool visitedScene;
+        public Vector3 position;
+        public Vector3 scale;
+    }
+
+    public struct SenecaRadioTower
+    {
+        public bool visitedScene;
+        public Vector3 position;
+        public Vector3 scale;
+    }
+
+    public struct SenecaMeadow
      {
         public bool visitedScene;
         public Vector3 position;
         public Vector3 scale;
      }
 
-     public struct SenecaRoad
-     {
+    public struct UtanCampsite
+    {
         public bool visitedScene;
         public Vector3 position;
         public Vector3 scale;
-     }
+    }
 
-     public struct UtanMeadow
-     {
+    public struct UtanFork
+    {
         public bool visitedScene;
         public Vector3 position;
         public Vector3 scale;
-     }
+    }
 
-     public struct UtanRoad
-     {
+    public struct UtanFarm
+    {
         public bool visitedScene;
         public Vector3 position;
         public Vector3 scale;
-     }
+    }
 
-     public struct UtanFork
-     {
+    public struct UtanHunterCamp
+    {
         public bool visitedScene;
         public Vector3 position;
         public Vector3 scale;
-     }
+    }
 
-	public struct UtanRadioTower
-	{
-		public bool visitedScene;
-		public Vector3 position;
-		public Vector3 scale;
-	}
+    public struct UtanRocks
+    {
+        public bool visitedScene;
+        public Vector3 position;
+        public Vector3 scale;
+    }
+
+    public struct UtanRoad
+    {
+        public bool visitedScene;
+        public Vector3 position;
+        public Vector3 scale;
+    }
+
+    public struct UtanRadioTower
+    {
+        public bool visitedScene;
+        public Vector3 position;
+        public Vector3 scale;
+    }
+
+    public struct UtanMeadow
+    {
+        public bool visitedScene;
+        public Vector3 position;
+        public Vector3 scale;
+    }
 }
--- SENECA/Assets/Scripts/Game_World/Exit.cs
+++ SENECA/Assets/Scripts/Game_World/Exit.cs
@@ -9,20 +9,20 @@ public class Exit : MonoBehaviour
 	public const string SENECA_CAMPSITE = "To_SenecaCampsite";
 	public const string SENECA_FARM = "To_SenecaFarm";
 	public const string SENECA_FOREST_FORK= "To_SenecaForestFork";
-	public const string SENECA_Huntercamp = "To_SenecaHuntercamp";
+	public const string SENECA_HUNTER_CAMP = "To_SenecaHuntercamp";
 	public const string SENECA_MEADOW = "To_SenecaMeadow";
-	public const string SENECA_RADIOTOWER = "To_SenecaRadiotower";
+	public const string SENECA_RADIO_TOWER = "To_SenecaRadiotower";
 	public const string SENECA_ROAD = "To_SenecaRoad";
-	public const string SENECA_ROCK = "To_SenecaRocks";
+	public const string SENECA_ROCKS = "To_SenecaRocks";
 
 	public const string UTAN_CAMPSITE = "To_UtanCampsite";
 	public const string UTAN_FARM = "To_UtanFarm";
 	public const string UTAN_FOREST_FORK = "To_UtanForestFork";
-	public const string UTAN_Huntercamp = "To_UtanHuntercamp";
+	public const string UTAN_HUNTER_CAMP = "To_UtanHuntercamp";
 	public const string UTAN_MEADOW = "To_UtanMeadow";
-	public const string UTAN_RADIOTOWER = "To_UtanRadioTower";
+	public const string UTAN_RADIO_TOWER = "To_UtanRadioTower";
 	public const string UTAN_ROAD = "To_UtanRoad";
-	public const string UTAN_ROCK = "To_UtanRocks";
+	public const string UTAN_ROCKS = "To_UtanRocks";
 
 
 	public bool canTransferScene;
@@ -80,37 +80,91 @@ public class Exit : MonoBehaviour
 			TransitionData.Instance.SENECA_FORK.scale = player.localScale;
 			Services.Scenes.Swap<SenecaForestForkSceneScript>(TransitionData.Instance);
 		}
-		else if (nextScene == SENECA_MEADOW) 
+        else if (nextScene == SENECA_FARM)
+        {
+            TransitionData.Instance.SENECA_FARM.position = player.position;
+            TransitionData.Instance.SENECA_FORK.scale = player.localScale;
+            Services.Scenes.Swap<SenecaFarmSceneScript>(TransitionData.Instance);
+        }
+        else if (nextScene == SENECA_HUNTER_CAMP)
+        {
+            TransitionData.Instance.SENECA_HUNTER_CAMP.position = player.position;
+            TransitionData.Instance.SENECA_HUNTER_CAMP.scale = player.localScale;
+            Services.Scenes.Swap<SenecaHunterCampSceneScript>(TransitionData.Instance);
+        }
+        else if (nextScene == SENECA_ROCKS)
+        {
+            TransitionData.Instance.SENECA_ROCKS.position = player.position;
+            TransitionData.Instance.SENECA_ROCKS.scale = player.localScale;
+            Services.Scenes.Swap<SenecaForestForkSceneScript>(TransitionData.Instance);
+        }
+        else if (nextScene == SENECA_ROAD)
+        {
+            TransitionData.Instance.SENECA_ROAD.position = player.position;
+            TransitionData.Instance.SENECA_ROAD.scale = player.localScale;
+            Services.Scenes.Swap<SenecaRoadTowerSceneScript>(TransitionData.Instance);
+        }
+        else if (nextScene == SENECA_RADIO_TOWER)
+        {
+            TransitionData.Instance.SENECA_RADIO_TOWER.position = player.position;
+            TransitionData.Instance.SENECA_RADIO_TOWER.scale = player.localScale;
+            Services.Scenes.Swap<SenecaRadioTowerSceneScript>(TransitionData.Instance);
+        }
+        else if (nextScene == SENECA_MEADOW) 
 		{
 			TransitionData.Instance.SENECA_MEADOW.position = player.position;
 			TransitionData.Instance.SENECA_MEADOW.scale = player.localScale;
 			Services.Scenes.Swap<SenecaMeadowSceneSript>(TransitionData.Instance);
 		}
-		else if (nextScene == UTAN_MEADOW) 
-		{
-			TransitionData.Instance.UTAN_MEADOW.position = player.position;
-			TransitionData.Instance.UTAN_MEADOW.scale = player.localScale;
-			Services.Scenes.Swap<UtanMeadowSceneScript>(TransitionData.Instance);
-		}
-		else if (nextScene == UTAN_ROAD) 
-		{
-			TransitionData.Instance.UTAN_ROAD.position = player.position;
-			TransitionData.Instance.UTAN_ROAD.scale = player.localScale;
-			Services.Scenes.Swap<UtanRoadSceneScript>(TransitionData.Instance);
-		}
-		else if (nextScene == UTAN_FOREST_FORK) 
-		{
-			TransitionData.Instance.UTAN_FORK.position = player.position;
-			TransitionData.Instance.UTAN_FORK.scale = player.localScale;
-			Services.Scenes.Swap<UtanForkPathSceneScript>(TransitionData.Instance);
-		}
-		else if (nextScene == UTAN_RADIOTOWER) 
-		{
-			TransitionData.Instance.UTAN_RADIOTOWER.position = player.position;
-			TransitionData.Instance.UTAN_RADIOTOWER.scale = player.localScale;
-			Services.Scenes.Swap<UtanRadioTowerSceneScript>(TransitionData.Instance);
-		}
-	}
+        else if (nextScene == UTAN_CAMPSITE)
+        {
+            TransitionData.Instance.UTAN_CAMPSITE.position = player.position;
+            TransitionData.Instance.UTAN_CAMPSITE.scale = player.localScale;
+            Services.Scenes.Swap<UtanCampsiteSceneScript>(TransitionData.Instance);
+        }
+        else if (nextScene == UTAN_FOREST_FORK)
+        {
+            TransitionData.Instance.UTAN_FORK.position = player.position;
+            TransitionData.Instance.UTAN_FORK.scale = player.localScale;
+            Services.Scenes.Swap<UtanForkPathSceneScript>(TransitionData.Instance);
+        }
+        else if (nextScene == UTAN_FARM)
+        {
+            TransitionData.Instance.UTAN_FARM.position = player.position;
+            TransitionData.Instance.UTAN_FARM.scale = player.localScale;
+            Services.Scenes.Swap<UtanFarmSceneScript>(TransitionData.Instance);
+        }
+        else if (nextScene == UTAN_HUNTER_CAMP)
+        {
+            TransitionData.Instance.UTAN_HUNTER_CAMP.position = player.position;
+            TransitionData.Instance.UTAN_HUNTER_CAMP.scale = player.localScale;
+            Services.Scenes.Swap<UtanHunterCampSceneScript>(TransitionData.Instance);
+        }
+        else if (nextScene == UTAN_ROCKS)
+        {
+            TransitionData.Instance.UTAN_ROCKS.position = player.position;
+            TransitionData.Instance.UTAN_ROCKS.scale = player.localScale;
+            Services.Scenes.Swap<UtanRocksSceneScript>(TransitionData.Instance);
+        }
+        else if (nextScene == UTAN_ROAD)
+        {
+            TransitionData.Instance.UTAN_ROAD.position = player.position;
+            TransitionData.Instance.UTAN_ROAD.scale = player.localScale;
+            Services.Scenes.Swap<UtanRoadSceneScript>(TransitionData.Instance);
+        }
+        else if (nextScene == UTAN_RADIO_TOWER)
+        {
+            TransitionData.Instance.UTAN_RADIO_TOWER.position = player.position;
+            TransitionData.Instance.UTAN_RADIO_TOWER.scale = player.localScale;
+            Services.Scenes.Swap<UtanRadioTowerSceneScript>(TransitionData.Instance);
+        }
+        else if (nextScene == UTAN_MEADOW)
+        {
+            TransitionData.Instance.UTAN_MEADOW.position = player.position;
+            TransitionData.Instance.UTAN_MEADOW.scale = player.localScale;
+            Services.Scenes.Swap<UtanMeadowSceneScript>(TransitionData.Instance);
+        }
+    }
 
 
 }
