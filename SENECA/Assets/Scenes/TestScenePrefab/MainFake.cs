28696182a0cf8bf05b6690aa553e284d40ea4fed
mkim1994 <mkim2@andrew.cmu.edu>
2017-05-14 17:12:22 -0400

    test scene prefabs scene

---
+++ SENECA/Assets/Scenes/TestScenePrefab/MainFake.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using ChrsUtils.ChrsEventSystem.EventsManager;
+using GameSceneManagerSystem;
+using PrefabDataBase;
+
+public class MainFake : MonoBehaviour
+{
+    private void Awake()
+    {
+        Assert.raiseExceptions = true;
+
+        Services.Prefabs = Resources.Load<PrefabDB>("Prefabs/ScenePrefab");
+        Services.Events = new GameEventsManager();
+
+		Services.Scenes = new GameSceneManager<TransitionData>(gameObject, Services.Prefabs.Scenes);
+		Debug.Log ("debug");
+	
+		//Services.Scenes.PushScene<TitleSceneScript>();
+    }
+}
