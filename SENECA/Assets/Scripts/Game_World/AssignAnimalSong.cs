9e70a806945f21f05db56c3e26d68fda2426abd4
Christopher Williams <c.j.williams717@gmail.com>
2017-05-15 16:54:19 -0400

    Added animal songs via script

---
+++ SENECA/Assets/Scripts/Game_World/AssignAnimalSong.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssignAnimalSong : MonoBehaviour {
+
+    public AudioClip clip;
+    public AudioSource audioSource;
+	// Use this for initialization
+	void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (transform.name == "Road")
+        {
+            clip = Resources.Load("Audio/SFX/SFX/INDIVIDUAL PUZZLE NOTES/SecondPuzzle_AnimalSong") as AudioClip;
+        }
+        else
+        {
+            clip = Resources.Load("Audio/SFX/SFX/INDIVIDUAL PUZZLE NOTES/FirstPuzzle_AnimalSong") as AudioClip;
+        }
+
+        audioSource.PlayOneShot(clip);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
