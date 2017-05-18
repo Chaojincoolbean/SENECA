4e14c7a3ebe50a1dfc2e4009ef6316aef0dcdf45
mkim1994 <mkim2@andrew.cmu.edu>
2017-05-18 17:21:54 -0400

    title screen animation

---
+++ SENECA/Assets/ParticleAnimation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleAnimation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<ParticleSystem>().GetComponent<Renderer>().sortingLayerName = "particle";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
